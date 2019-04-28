3d2623f1dd5a9a9f8f944212c6b2551f821638e3
jjmeg <flightlessbird-go@hotmail.com>
2019-04-28 23:03:12 +0800

    feat:add video api

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
+	"videoserver/api/defs"
+	"videoserver/api/utils"
 )
 
 //
@@ -52,6 +55,75 @@ func DeleteUser(loginName string, pwd string) error {
 	}
 	_, err = stmtDel.Exec(loginName, pwd)
 	if err != nil {
+		return err
+	}
+	defer stmtDel.Close()
+	return nil
+}
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	//create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	//creatime->db
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05") //M D y, HH:MM:SS
+	stmtIns, err := dbConn.Prepare(
+		`INSERT INTO video_info (id, author_id,name,display_ctime) VALUES (?,?,?,?)`)
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &defs.VideoInfo{
+		Id:           vid,
+		AuthorId:     aid,
+		Name:         name,
+		DisplayCtime: ctime,
+	}
+	defer stmtIns.Close()
+	return res, nil
+}
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare(`SELECT author_id,name,display_ctime FROM video_info WHERE Id = ?`)
+	var aid int
+	var dct string
+	var name string
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows {
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{
+		Id:           vid,
+		AuthorId:     aid,
+		Name:         name,
+		DisplayCtime: dct,
+	}
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string)error  {
+	stmtDel,err := dbConn.Prepare("DELETE FROM video_info WHERE Id = ?")
+	if err !=nil{
+		return err
+	}
+	
+	_,err = stmtDel.Exec(vid)
+	if err !=nil{
 		return err
 	}
 	defer stmtDel.Close()
