bac69e2967ab71e6974a31d7f8bf8164e373d9d0
jjmeg <flightlessbird-go@hotmail.com>
2019-09-18 23:16:43 +0800

    feat: fulfill taskrunner

--- scheduler/dbops/internal.go
+++ scheduler/dbops/internal.go
@@ -1,1 +1,50 @@
 package dbops
+
+import "log"
+
+// api->videoid->db
+// dispathcer -> mysql -> videoid->datachannel
+// executor -> datachannel -> videoid -> delete videos
+
+func ReadVideoDeletionRecord(count int) ([]string, error) {
+	stmtOut, err := dbConn.Prepare("SELECT video_id FROM video_del_rec LIMIT ?")
+
+	var ids []string
+
+	if err != nil {
+		return ids, err
+	}
+
+	rows, err := stmtOut.Query(count)
+	if err != nil {
+		log.Printf("Query VideoDeletionRecord Error:%v", err)
+		return ids, nil
+	}
+
+	for rows.Next() {
+		var id string
+		if err := rows.Scan(&id); err != nil {
+			return ids, err
+		}
+
+		ids = append(ids, id)
+	}
+	defer stmtOut.Close()
+	return ids, nil
+}
+
+func DelVideoDeletionRecord(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_del_rec WHERE video_id=?")
+
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
+	if err != nil {
+		log.Printf("Deleting VideoDeletionRecord Error:%v", err)
+		return err
+	}
+	defer stmtDel.Close()
+	return nil
+}
